b1f662f882f4106e403068cd785259150b101c9b
Daniel Kirchner <daniel@ekpyron.org>
2015-07-18 05:04:13 +0200

    Change to MIT license.

--- oglp/query.h
+++ oglp/query.h
@@ -1,19 +1,22 @@
 /*
- * This file is part of oglp.
+ * Copyright 2015 Daniel Kirchner
  *
- * oglp is free software: you can redistribute it and/or modify
+ * This file is part of midium.
+ *
+ * midium is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * oglp is distributed in the hope that it will be useful,
+ * midium is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with oglp.  If not, see <http://www.gnu.org/licenses/>.
+ * along with midium.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef OGLP_QUERY_H
 #define OGLP_QUERY_H
 
